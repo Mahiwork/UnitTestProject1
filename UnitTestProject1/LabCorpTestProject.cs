7b0b527525b4666ae8d3037e940b52ccb7f313a2
Mahimur Rasheed <Mahimur.Rasheed@taxact.com>
2023-07-03 09:25:37 -0500

    Add project files.

---
+++ UnitTestProject1/LabCorpTestProject.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+using UnitTestProject1.POM;
+
+namespace UnitTestProject1
+{
+
+    [TestClass]
+    public class LabCorpTestProject
+    {
+
+
+        /// <summary>
+        /// Test Method Navigates to Lab Corp, searches specific job function, then confirms Job Title, Location, and ID using Chrome
+        /// </summary>
+        [TestMethod]
+        public void ValidateJobTitleLocationID()
+
+        {
+            // Creates an Instance of web driver and navigates to Labcorp website
+            IWebDriver webDriver = new ChromeDriver(@"C:\chromedriver_win32");
+            webDriver.Navigate().GoToUrl("https://www.labcorp.com/");
+
+            // Sets implicit wait so that the webdriver waits a duration of time to seek elements on a page
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+            LocatorClass Locator = new LocatorClass(webDriver);
+
+
+            //Given user navigates to jobs, browses for Software Development Manager
+            Locator.Classes.Click();
+            Locator.SearchButton.SendKeys("Software Development Manager");
+            Locator.SearchClick.Click();
+
+            //When user selects Software Manager
+            Locator.SoftwareManager.Click();
+
+
+            //Then user will see job characteristics such as id, location, and title 
+            Assert.AreEqual("Software Development Manager", Locator.JobTitle);
+            Assert.AreEqual("Location\r\nSan Antonio, Texas, United States of America", Locator.JobLocation);
+            Assert.AreEqual("Job Id : 2231421", Locator.JobId);
+            Assert.AreEqual("Category\r\nInformation Technology", Locator.JobCategory);
+            Assert.AreEqual("Job Type\r\nFull-Time", Locator.JobType);
+            Assert.AreEqual("Labcorp Employee? Apply Here", Locator.AlreadyEmployee);
+
+            //Information is displayed in console after text is stored in strings
+            Console.Write(Locator.JobTitle);
+            Console.Write(Locator.JobLocation);
+            Console.Write(Locator.JobId);  
+            Console.Write(Locator.JobCategory);
+            Console.Write(Locator.JobType);
+            Console.Write(Locator.AlreadyEmployee);      
+
+            //User selects apply now
+            Locator.ApplyNow.Click();
+
+
+
+            webDriver.Close();
+        
+            
+
+            
+        }
+    }
+}
---
+++ UnitTestProject1/POM/LocatorClass.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace UnitTestProject1.POM
+{
+    public class LocatorClass
+    {
+
+        //Constructor for class
+        public LocatorClass(IWebDriver webDriver)
+        {
+            Driver = webDriver;
+        }
+
+
+
+        private IWebDriver Driver { get; }
+
+        //Web Elements 
+        public IWebElement Classes => Driver.FindElement(By.ClassName("no-ext"));
+        
+        public IWebElement SearchButton => Driver.FindElement(By.ClassName("ph-a11y-search-box"));
+
+        public IWebElement SearchClick => Driver.FindElement(By.Id("ph-search-backdrop"));
+
+        public IWebElement SoftwareManager => Driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div/div/div[2]/section[3]/div/div/div/div[2]/div[2]/ul/li[1]/div[1]/span/a/div/span"));
+
+        public IWebElement ApplyNow => Driver.FindElement(By.ClassName("primary-button"));
+
+        //String variables for assertions 
+        public string JobTitle => Driver.FindElement(By.ClassName("job-title")).Text;
+        
+        public String JobId => Driver.FindElement(By.ClassName("jobId")).Text;
+        public string JobLocation => Driver.FindElement(By.ClassName("job-location")).Text;
+
+        public string JobCategory=> Driver.FindElement(By.ClassName("job-category")).Text;
+
+        public string JobType => Driver.FindElement(By.ClassName("type")).Text;
+        public string AlreadyEmployee => Driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div[1]/div[2]/section/div/div/div/div[2]/a/ppc-content")).Text;
+
+
+
+
+
+
+
+
+    }
+
+}
